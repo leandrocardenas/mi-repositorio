474df9c7fcd99bfd1fbc30917da50db89954a137
unknown <leandrojobs11@gmail.com>
2022-09-14 22:21:49 -0300

    añadido de grid con bootstrap, carrusel con grid, y menu hamburguesa con java script, en pagina inicio y productos.

---
+++ menu.js
@@ -0,0 +1,8 @@
+let paginas = document.querySelector('#paginas');
+let menu = document.querySelector('#menu-bar');
+
+    menu.addEventListener('click', function() {
+    
+    paginas.classList.toggle('paginasMover');
+
+})
