674c028d07e0accef52258da1649f7771858e75a
unknown <leandrojobs11@gmail.com>
2022-11-14 23:34:16 -0300

    simulacion de carrito de compras

--- index.js
+++ index.js
@@ -1,56 +1,79 @@
-const suma =(parametro1,parametro2)=> {
-    resultado = parametro1 + parametro2;
-    return resultado;
+const productos = [
+    {nombre: `leche `, precio: 150},
+    {nombre: `bizcochitos `, precio: 120 },
+    {nombre: `pepsi `, precio: 300 },
+    {nombre: `cerveza `, precio: 400 },
+    {nombre: `levadura `, precio: 250}
+
+];
+
+let carrito = []
+
+let seleccion = prompt ("Buenas, quiere realizar una compra? si o no")
+
+while(seleccion != `si` && seleccion != `no`) {
+    alert(`Porfavor, ingrese si o no`)
+    seleccion = prompt ("Buenas, quiere realizar una compra? si o no")
 }
 
-const resta =(parametro1,parametro2)=> {
-    resultado = parametro1 - parametro2;
-    return resultado;
-}
+if(seleccion==`si`) {
+    alert(`te mostraremos la lista de productos disponibles`);
+    
+    let listaProductos = productos.map((producto) => producto.nombre + "" + producto.precio + `$`);
 
-const mult =(parametro1,parametro2)=> {
-    resultado = parametro1 * parametro2;
-    return resultado;
-}
+    alert(listaProductos.join(`, `));
 
-const div =(parametro1,parametro2)=> {
-    resultado = parametro1 / parametro2;
-    return resultado;
-}
-
-const poten =(parametro1,parametro2)=> {
-    resultado = parametro1 ** parametro2;
-    return resultado;
-}
-
-
-const calculadora =(parametro1,parametro2,operacion)=>{
-    let resultado
-    if(operacion === 'suma'){
-        resultado = suma(parametro1,parametro2);
-    }
-    else if (operacion === 'resta'){
-        resultado = resta(parametro1,parametro2); 
-    }
-    else if (operacion === 'mult'){
-        resultado = mult(parametro1,parametro2);
-    }
-    else if (operacion === 'div'){
-        resultado = div(parametro1,parametro2);
+    }else if (seleccion == `no` ) {
+        alert(`Hasta pronto!`);
     }
 
-    else if (operacion === 'poten'){
-        resultado = poten(parametro1,parametro2);
+while(seleccion != `no`) {
+    let producto = prompt (`agrega el producto que desees llevar a tu carrito`);
+    let precio = 0;
+
+    if(producto == `leche` || producto == `bizcochitos` || producto == `pepsi` || producto == `cerveza` || producto == `levadura`) {
+        switch(producto) {
+            case `leche`:
+            precio = 150
+            break;  
+            
+            case `bizcochitos`:
+            precio = 120
+            break; 
+
+            case `pepsi`:
+            precio = 300
+            break; 
+
+            case `cerveza`:
+            precio = 400
+            break; 
+
+            case `levadura`:
+            precio = 250
+            break;
+        }
+    let unidades = parseInt(prompt(`Cuantas unidades quiere llevar?`));
+
+    carrito.push({producto, unidades, precio});
+    console.log(carrito);
+    } else {
+        alert(`No poseemos ese producto. Por favor eliga un producto disponible`)
     }
 
 
-    return resultado;
+    seleccion = prompt(`desea continuar comprando?`)
 
+    while(seleccion == `no`){
+        alert(`Gracias por su compra! A continuacion veras tu carrito de compras.`);
+        carrito.forEach((carritoFinal) => {
+            alert(`producto: ${carritoFinal.producto}, unidades: ${carritoFinal.unidades}, valor total: ${carritoFinal.unidades * carritoFinal.precio}`)
+        });
+        break;
+    }
+    
 }
 
-const numero1 = parseInt(prompt('ingresa un numero'));
-const numero2 = parseInt(prompt('ingresa otro numero'));
-const oper = prompt('escribe la operacion que quieras realizar: suma, resta, mult, div o poten');
+const total = carrito.reduce ((acc,el) => acc + el.precio * el.unidades, 0);
+alert(`el total de tu compra es $${total}, que disfrutes tu compra!`);
 
-const resultadoCalculadora = calculadora(numero1,numero2,oper);
-alert(`el resultado es ${resultadoCalculadora}`);
