70baae672916be5032ef69759d97a187917bf8e3
unknown <leandrojobs11@gmail.com>
2022-11-28 22:56:55 -0300

    carrito de compras

--- index.js
+++ index.js
@@ -1,79 +1,89 @@
+const shopContenido = document.getElementById("shopContenido");
+const verCarrito = document.getElementById("verCarrito");
+const modalContenido = document.getElementById("modalContenido");
+
 const productos = [
-    {nombre: `leche `, precio: 150},
-    {nombre: `bizcochitos `, precio: 120 },
-    {nombre: `pepsi `, precio: 300 },
-    {nombre: `cerveza `, precio: 400 },
-    {nombre: `levadura `, precio: 250}
+    {id: 1, nombre: `leche `, precio: 150, img:"https://media.istockphoto.com/id/960341526/es/foto/cart%C3%B3n-de-leche.jpg?s=612x612&w=0&k=20&c=nqKZiOKRE6mT1nA_32MAQR7eg4nRxhRHTGiXiiboYdw=" },
+    {id: 2, nombre: `bizcochitos `, precio: 120, img:"https://www.todohumoonline.com.ar/wp-content/uploads/2020/11/7790957000668_02.jpg" },
+    {id: 3, nombre: `pepsi `, precio: 300, img:"https://m.media-amazon.com/images/I/51-r9pOh08L._SL1500_.jpg" },
+    {id: 4, nombre: `cerveza `, precio: 400, img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvKPKqTJWh5kqyWi_iQlV3Pw8qmzGZ8GRoUw&usqp=CAU" },
+    {id: 5, nombre: `harina `, precio: 250, img:"https://cordoba.parodisrl.com.ar/39004-thickbox_default/harina-blancaflor-leudante-1kg.jpg" }
 
 ];
 
-let carrito = []
+let carrito = [];
 
-let seleccion = prompt ("Buenas, quiere realizar una compra? si o no")
+productos.forEach((producto)=> {
+    let contenido = document.createElement("div");
+    contenido.className = "card";
+    contenido.innerHTML = `
+        <img src="${producto.img}">
+        <h3>${producto.nombre}</h3>
+        <p class="precio">${producto.precio}$</p>
+    `;
 
-while(seleccion != `si` && seleccion != `no`) {
-    alert(`Porfavor, ingrese si o no`)
-    seleccion = prompt ("Buenas, quiere realizar una compra? si o no")
-}
+    shopContenido.append(contenido);
 
-if(seleccion==`si`) {
-    alert(`te mostraremos la lista de productos disponibles`);
-    
-    let listaProductos = productos.map((producto) => producto.nombre + "" + producto.precio + `$`);
+    let comprar = document.createElement("button");
+    comprar.innerText = "Comprar";
+    comprar.className = "comprar";
 
-    alert(listaProductos.join(`, `));
+    contenido.append(comprar);
 
-    }else if (seleccion == `no` ) {
-        alert(`Hasta pronto!`);
-    }
-
-while(seleccion != `no`) {
-    let producto = prompt (`agrega el producto que desees llevar a tu carrito`);
-    let precio = 0;
-
-    if(producto == `leche` || producto == `bizcochitos` || producto == `pepsi` || producto == `cerveza` || producto == `levadura`) {
-        switch(producto) {
-            case `leche`:
-            precio = 150
-            break;  
-            
-            case `bizcochitos`:
-            precio = 120
-            break; 
-
-            case `pepsi`:
-            precio = 300
-            break; 
-
-            case `cerveza`:
-            precio = 400
-            break; 
-
-            case `levadura`:
-            precio = 250
-            break;
-        }
-    let unidades = parseInt(prompt(`Cuantas unidades quiere llevar?`));
-
-    carrito.push({producto, unidades, precio});
-    console.log(carrito);
-    } else {
-        alert(`No poseemos ese producto. Por favor eliga un producto disponible`)
-    }
-
-
-    seleccion = prompt(`desea continuar comprando?`)
-
-    while(seleccion == `no`){
-        alert(`Gracias por su compra! A continuacion veras tu carrito de compras.`);
-        carrito.forEach((carritoFinal) => {
-            alert(`producto: ${carritoFinal.producto}, unidades: ${carritoFinal.unidades}, valor total: ${carritoFinal.unidades * carritoFinal.precio}`)
+    comprar.addEventListener("click", ()=>{
+        carrito.push({
+            id: producto.id,
+            img: producto.img,
+            nombre: producto.nombre,
+            precio: producto.precio
         });
-        break;
-    }
-    
-}
+        console.log(carrito);
+    });
 
-const total = carrito.reduce ((acc,el) => acc + el.precio * el.unidades, 0);
-alert(`el total de tu compra es $${total}, que disfrutes tu compra!`);
+
+});
+
+verCarrito.addEventListener("click",()=> {
+    modalContenido.innerHTML = "";
+    modalContenido.style.display = "flex";
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header";
+    modalHeader.innerHTML = `
+    <h1 class="modal-header-title">Carrito</h1>
+    `;
+    modalContenido.append(modalHeader);
+
+    
+    
+    const modalButton = document.createElement("h1");
+    modalButton.innerText= "x";
+    modalButton.className = "modal-header-button";
+
+    modalButton.addEventListener("click", ()=> {
+        modalContenido.style.display = "none";
+    });
+
+    modalHeader.append(modalButton);
+
+
+    
+    carrito.forEach((producto)=>{
+    let carritoContenido = document.createElement("div");
+    carritoContenido.className = "modal-content"
+    carritoContenido.innerHTML =`
+        <img src="${producto.img}">
+        <h3>${producto.nombre}</h3>
+        <p>${producto.precio}$</p>
+    `;
+
+    modalContenido.append(carritoContenido);
+    });
+    
+    const total = carrito.reduce((acc, unid)=> acc + unid.precio, 0);
+
+    const totalCompra = document.createElement("div");
+    totalCompra.className = "total-contenido";
+    totalCompra.innerHTML = `total a pagar: ${total}$`;
+    modalContenido.append(totalCompra);
+});
 
